149c32ca85f11bd790500398e8c7554eb3995465
Tonatiu Cruz Bautista <tonatiu_98x@hotmail.com>
2025-10-31 12:30:26 -0600

    refactor side menu options to use dynamic menu options and update HTML structure

--- src/app/gifs/components/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
   styleUrl: './side-menu-options.css',
 })
 export class SideMenuOptions {
-
+  menuOptions: MenuOption[] = [{
+    label: 'Trending',
+    subLabel: 'Gifs Populares',
+    route: '/dashboard/trending',
+    icon: 'fa-solid fa-chart-line'
+  }, {
+    label: 'Trending',
+    subLabel: 'Buscar Gifs',
+    route: '/dashboard/search',
+    icon: 'fa-solid fa-magnifying-glass'
+  }];
 }
