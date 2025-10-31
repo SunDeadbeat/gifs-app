d53656f52ab1add0c20a42c7cd0ef1ea52192fc0
Tonatiu Cruz Bautista <tonatiu_98x@hotmail.com>
2025-10-31 16:21:04 -0600

    feat: add gif list component with HTML and CSS files

---
+++ src/app/gifs/components/gif-list/git-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-list',
+  imports: [],
+  templateUrl: './gif-list.html',
+  styleUrl: './gif-list.css',
+})
+export class GifList {
+
+}
