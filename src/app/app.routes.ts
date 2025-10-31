a49dc9b9605e9a371f270e73e430c4ecb2a79cd3
Tonatiu Cruz Bautista <tonatiu_98x@hotmail.com>
2025-10-31 10:43:52 -0600

    add trending page with routing and initial implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ export const routes: Routes = [{
     path: 'dashboard',
     loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page'),
 }, {
+    path: 'trending',
+    loadComponent: () => import('./gifs/pages/trending-page/trending-page'),
+}, {
     path: '**',
     redirectTo: 'dashboard',
 }];
