5255e3a640121c8c81a6bd1766078727d01332e2
Tonatiu Cruz Bautista <tonatiu_98x@hotmail.com>
2025-10-31 16:28:02 -0600

    feat: implement gif list and trending page components with updated HTML structure

--- src/app/gifs/components/gif-list/git-list.ts
+++ src/app/gifs/components/gif-list/git-list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GifListItem } from "./gif-list-item/gif-list-item";
 
 @Component({
   selector: 'app-gif-list',
-  imports: [],
+  imports: [GifListItem],
   templateUrl: './gif-list.html',
   styleUrl: './gif-list.css',
 })
--- src/app/gifs/pages/trending-page/trending-page.ts
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GifList } from "../../components/gif-list/git-list";
 
 @Component({
   selector: 'app-trending-page',
-  imports: [],
+  imports: [GifList],
   templateUrl: './trending-page.html',
   styleUrl: './trending-page.css',
 })
